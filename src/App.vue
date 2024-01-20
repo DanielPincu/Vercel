56d503f92492f2f6f8f831ce4cc527d416a4546c
DanielPincu <ghepin01@easv365.dk>
2024-01-20 21:08:47 +0100

    Comit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import AuthPage from "./pages/AuthPage/index.vue";
+import ChatsPage from "./pages/ChatsPage/index.vue";
+</script>
+
+<template>
+  <AuthPage v-if="!user" @onAuth="handleAuth" />
+  <ChatsPage
+    v-else
+    v-bind:username="user.username"
+    v-bind:secret="user.secret"
+  />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: undefined,
+    };
+  },
+  methods: {
+    handleAuth(user) {
+      this.user = user;
+    },
+  },
+};
+</script>
+<kbd></kbd>
+
